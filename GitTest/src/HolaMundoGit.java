d00a61ae694885d8e3fefe94005aa473e6793747
microAlumno <microalumno@gmail.com>
2013-02-20 09:25:04 +0100

    Commit Inicial

---
+++ GitTest/src/HolaMundoGit.java
@@ -0,0 +1,12 @@
+
+public class HolaMundoGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Se va a enviar una confirmacion inicial a GIT");
+
+	}
+
+}
