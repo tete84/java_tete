9a071871da0e3a4db7b118e6af367c82032999ca
microAlumno <microalumno@gmail.com>
2013-02-20 09:27:07 +0100

    Modificacion de la salida del programa

--- GitTest/src/HolaMundoGit.java
+++ GitTest/src/HolaMundoGit.java
@@ -6,6 +6,7 @@ public class HolaMundoGit {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Se va a enviar una confirmacion inicial a GIT");
+		System.out.println("Modificando por commit");
 
 	}
 
